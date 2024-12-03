f4e23ba601055b3722195c21f2a0dda7403c76ff
Basma khan <basmakhan672@gmail.com>
2024-12-03 14:23:47 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='heading'>CSS GRID
+    <div className='div'>
+    <div className='main'>
+    
+      <div className='a grid-item'>A</div>
+      <div className="b grid-item ">B</div>
+      <div className="c grid-item">C</div>
+      <div className="d grid-item">D</div>
+      <div className="e grid-item">E</div>
+      <div className="f grid-item">F</div>
+      <div className="g grid-item">G</div>
+      <div className="h grid-item">H</div>
+      <div className="i grid-item">I</div>
+     
+    </div>
+    </div>
+    </div>
+   
+  )
+}
+
+export default page
